ca1e39bc9766d5a07552a202d52f171d4bef8e49
Tsvetina Kalcheva <cvetinakalcheva@gmail.com>
2020-09-28 20:47:48 +0300

    Add QuestionRepositoryTest. Required Answer.question to be set.

--- src/main/java/bg/startit/spring/quiz/model/Answer.java
+++ src/main/java/bg/startit/spring/quiz/model/Answer.java
@@ -32,5 +32,6 @@ public class Answer {
     @Max(10)
     private int score;
     @ManyToOne
+    @NotNull
     private Question question;
 }
--- src/main/java/bg/startit/spring/quiz/repository/AnswerRepository.java
+++ src/main/java/bg/startit/spring/quiz/repository/AnswerRepository.java
@@ -1,9 +1,13 @@
 package bg.startit.spring.quiz.repository;
 
 import bg.startit.spring.quiz.model.Answer;
+import bg.startit.spring.quiz.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestion(Question question);
 }
